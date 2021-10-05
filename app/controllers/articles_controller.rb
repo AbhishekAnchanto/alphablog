b7cfb0352c90d728d60fb43af1e4322e37b403dd
Abhishek <abhishek.sharma@anchanto.com>
2021-10-05 17:23:07 +0530

    initial commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -48,14 +48,25 @@ end
  def update
     @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article was updated successfully."
+      #flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
       render 'edit'
     end
 
 
-end
+  end
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path 
+
+  end
+
+
+
 
     
 end
