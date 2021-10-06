37f748cc42427a33d3977186e34d5fbcf06379a3
Abhishek <abhishek.sharma@anchanto.com>
2021-10-06 10:26:32 +0530

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
-class User< ApplicationRecord
+class User < ApplicationRecord
 
-#attr_accessor :username, :email
-
-
-end
+    validates :username, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { maximum: 105 },
+                        format: { with: VALID_EMAIL_REGEX }
+  end 
