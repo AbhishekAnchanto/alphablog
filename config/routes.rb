fcde254ed68ae952a3c296e8e809b3696df0c51b
Abhishek <abhishek.sharma@anchanto.com>
2021-10-06 16:31:33 +0530

    new

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles  #only: [:show, :index, :new , :create ,:edit, :update]
+  get'signup' ,to: 'users#new'
+  
+  resources :users, except: [:new]
+
+
   
    
   
--- db/migrate/20211006032620_create_users.rb
+++ db/migrate/20211006032620_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
 
       t.string :username
       t.string :email
-       t.timestamps
+       t.timestamps 
     end
   end
 end
