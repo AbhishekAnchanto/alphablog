d11b39b2568baaec65a444ef8cbbb554af1d068d
Abhishek <abhishek.sharma@anchanto.com>
2021-10-08 10:06:48 +0530

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
-    
-end
+    def current_blogger
+       @current_blogger ||= Blogger.find(session[:blogger_id]) if session[:blogger_id]
+    end
+ 
+    def blogger_signed_in?
+       if session[:blogger_id]==nil
+          puts session[:blogger_id]
+          redirect_to root_path
+       else
+          !!current_blogger
+       end
+    end
+ 
+    # def require_user
+    #   if !blogger_signed_in?
+    #      flash[:alert]="You must be logged in to perform that action"
+    #      redirect_to new_blogger_session_path
+    #   end
+    # end
+ 
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   
+  devise_for :bloggers,  controllers: { sessions: 'bloggers/sessions', registrations: "bloggers/registrations" } 
   root 'pages#home'
   get 'about', to: 'pages#about'
+ 
 
   resources :articles  #only: [:show, :index, :new , :create ,:edit, :update]
   get'signup' ,to: 'users#new'
   
   resources :users, except: [:new]
+  resources :bloggers
 
 
   
