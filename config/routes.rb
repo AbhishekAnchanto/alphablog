bbd933ffd9c1f84490a081d18292c8c64012465f
Abhishek <abhishek.sharma@anchanto.com>
2021-10-12 13:16:39 +0530

    indentation_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  
   devise_for :bloggers
   root 'pages#home'
   get 'about', to: 'pages#about'
- 
-
   resources :articles  #only: [:show, :index, :new , :create ,:edit, :update]
   get'signup' ,to: 'users#new'
-  
   resources :users, except: [:new]
-
   resources :bloggers , only: [:show ,:index]
-
   resources :comments, only: [:new,:create]
-
-  
-   
-  
 end
